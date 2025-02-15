e8e020bcad2a126e5c10ca8addd867ae3d6fb895
clogol <clementogol@gmail.com>
2025-02-15 15:21:12 +0300

    counts alphabets

--- alphacount.go
+++ alphacount.go
@@ -6,12 +6,18 @@ func AlphaCount(s string) int {
 	count := 0
 	runeS := []rune(s)
 	for i := 0; i < len(runeS); i++ {
-		if runeS[i] >= 'A' && runeS[i] <= 'Z' {
+
+		if (runeS[i] >= 'A' && runeS[i] <= 'Z') ||
+			(runeS[i] >= 'a' && runeS[i] <= 'z') {
 			count++
 		}
-		if runeS[i] >= 'a' && runeS[i] <= 'z' {
-			count++
-		}
+
+		// if runeS[i] >= 'A' && runeS[i] <= 'Z' {
+		// 	count++
+		// }
+		// if runeS[i] >= 'a' && runeS[i] <= 'z' {
+		// 	count++
+		// }
 	}
 	return count
 }
